c806a197831314b03456afbbbc33533731ba7755
AliBagherz <Ali.Bagherzadeh@digikala.com>
2023-05-25 16:34:25 +0330

    Add models and urls of interview app

--- interview/models.py
+++ interview/models.py
@@ -11,3 +11,30 @@ class Task(BaseModel):
         User, on_delete=models.DO_NOTHING, related_name="tasks", null=False, blank=False
     )
 
+
+class Assignment(BaseModel):
+    task = models.ForeignKey(
+        Task, on_delete=models.CASCADE, related_name='assignments', null=False, blank=False
+    )
+    email = models.EmailField(null=False)
+    expiration_date = models.DateTimeField()
+
+
+class TaskQuestion(BaseModel):
+    task = models.ForeignKey(
+        Task, on_delete=models.CASCADE, related_name='task_questions', null=False, blank=False
+    )
+    question = models.ForeignKey(
+        'question.models.question', on_delete=models.CASCADE, related_name='task_questions', null=False, blank=False
+    )
+    score = models.IntegerField(null=False)
+
+
+class AssignmentGrade(BaseModel):
+    assignment = models.ForeignKey(
+        Assignment, on_delete=models.CASCADE, related_name='assignment_grades', null=False, blank=False
+    )
+    question = models.ForeignKey(
+        'question.models.question', on_delete=models.CASCADE, related_name='assignment_grades', null=False, blank=False
+    )
+    grade = models.IntegerField(null=False)
